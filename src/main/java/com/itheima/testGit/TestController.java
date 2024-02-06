87b327d2d6924b705a7fd20314f3e51d0fbc97ca
lxy <1376922411@qq.com>
2024-02-06 13:49:47 +0800

    test01

---
+++ src/main/java/com/itheima/testGit/TestController.java
@@ -0,0 +1,11 @@
+package com.itheima.testGit;
+
+
+
+
+public class TestController {
+    public static final String WORD_TEST="wordTest";
+
+}
+
+
--- src/test/java/com/itheima/TestGitApplicationTests.java
+++ src/test/java/com/itheima/TestGitApplicationTests.java
@@ -1,4 +1,4 @@
-package com.itheima.testgit;
+package com.itheima;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
